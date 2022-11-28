fa4f6998a389b62f2ffc2e46eed5f66fc20e78f5
arbaazkhanrs <arbaazkhanrss@gmail.com>
2022-11-28 15:44:50 +0530

    Hello

---
+++ footer.php
@@ -0,0 +1,55 @@
+
+<head>
+  <style>
+
+footer {
+
+  
+    font-family: Montserrat, sans-serif;
+    margin-bottom: 0;
+    background-color: lightblue;
+    border: 0;
+    font-size: 18px !important;
+    letter-spacing: 2px;
+    opacity: 5;
+    padding: 10px;
+    border-radius: 0;
+
+    }
+  </style>
+<body>
+  <br>
+  <br>
+  <br>
+  <br>
+<br>
+  <br>
+<br>
+  <br>
+<br>
+  <br>
+
+<footer class="text-center">
+ 
+
+  
+<div align="right"></div>
+ 
+    <br>
+  <h3><a style="color: #000; text-decoration: none"><b>&nbspALPHA TRAVELS
+<br> © 2022, Empire , Developed by Arbaazkhan</a></b></h3>
+  
+
+
+</div>
+</footer>
+
+
+<footer>
+  
+        <div class="copyright">
+           
+
+        </div>
+    </footer>
+</body></head>
